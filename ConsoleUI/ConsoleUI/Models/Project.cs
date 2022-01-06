86560acda36e19a933057f5db6f024e84edf1eb9
Harmmike <harmmike46@gmail.com>
2022-01-06 12:40:51 +0800

    Removed console project from mobile version

--- ConsoleUI/ConsoleUI/Models/Project.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleUI.Models
-{
-    public class Project
-    {
-        public string ProjectName { get; set; }
-        public string ProjectDescription { get; set; }
-        public int PomodoroCount { get; set; }
-    }
-}
